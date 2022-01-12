c66806227e07125a47c77d68dccb79a234a74156
Whatsapp0 <97558539+Whatsapp0@users.noreply.github.com>
2022-01-12 17:37:42 +0700

    Update and rename shop/sewa.js to toko/sewa.js

---
+++ toko/sewa.js
@@ -0,0 +1,24 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 3.000
+⬡ 2 MINGGU = Rp 6.000
+⬡ 3 MINGGU = Rp 8.000
+⬡ 1 BULAN = Rp 10.000
+⬡ PERMANEN = Rp 15.000
+
+ 
+
+
+⬣ MINAT? PM
+⬡ wa.me/6285712157431
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
